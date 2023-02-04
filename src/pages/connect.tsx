dfdaae5bfeee72ad7bf82a778cb51bc60ff25fe6
EHansonn <evanhanson02@gmail.com>
2023-02-04 17:39:39 -0500

    view user profiles

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -5,29 +5,32 @@ import { GetServerSideProps } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
 import { Card, Space, Col, Row, Button } from 'antd';
+import Link from 'next/link';
+import { User } from '@prisma/client';
 interface Props {
 	users: User[];
 }
-const Connect = ({ users }) => {
+
+const Connect: React.FC<Props> = ({ users }) => {
 	console.log(users);
 	return (
 		<div className="flex-col flex space-y-4 ">
 			<Space direction="vertical" size="middle" style={{ display: 'flex' }}>
 				{users.map((user, i) => (
-					<Card title={user.name} className="" size="small">
+					<Card key={i} title={user.name} className="" size="small">
 						<div className="flex ">
 							<img src={user.image} className="mt-3 bg-slate-500 rounded-full  h-[100px] w-[100px]" />
 							<div className="flex justify-evenly w-full">
 								<div className="flex flex-col">Bio: {user.bio}</div>
 								<div className="flex flex-col">
 									Hobbies:
-									{user.hobbies.map((hobby) => (
-										<div>{hobby.hobbyName}</div>
+									{user.hobbies.map((hobby, i) => (
+										<div key={i}>{hobby.hobbyName}</div>
 									))}
 								</div>
-								<Button className="content-center" type="primary">
+								<Link className="text-black mr-10 no-underline" href={`/users/${user.id}`}>
 									Connect
-								</Button>
+								</Link>
 							</div>
 						</div>
 					</Card>
@@ -42,7 +45,14 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ req, res }
 	const session = await getServerSession(req, res, authOptions);
 	if (session) {
 		const users = await prisma?.user.findMany({
-			select: { name: true, image: true, bio: true, hobbies: { include: { hobby: true } }, jobName: true },
+			select: {
+				id: true,
+				name: true,
+				image: true,
+				bio: true,
+				hobbies: { include: { hobby: true } },
+				jobName: true,
+			},
 		});
 
 		if (users) {
---
+++ src/pages/users/[[...id]].tsx
@@ -0,0 +1,145 @@
+import { useState } from 'react';
+import Bio from 'components/account-components/Bio';
+import Settings from 'components/account-components/Settings';
+import Trips from 'components/account-components/Trips';
+import RenderMap from '../../../components/maps/Map';
+import { useSession } from 'next-auth/react';
+import LocationSearch from 'components/maps/AutoComplete';
+import { UserGetResponse } from './api/user/[id]';
+import { GetServerSideProps, NextPage } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]';
+
+export type User = {
+	id: string;
+	bio: string;
+	pfp: string;
+	name: string;
+	job: string;
+	hobbies: string[];
+};
+export type Trip = {
+	desc: string;
+	location: string;
+	date: string;
+	lat: number;
+	lon: number;
+	urls: string[];
+};
+interface Props {
+	user?: User;
+	jobs?: string[];
+	hobbies?: string[];
+	trips?: Trip[];
+	error?: string;
+}
+
+const Account: NextPage<Props> = ({ user, error, hobbies, jobs, trips }) => {
+	const [displayedInfo, selectDisplayedInfo] = useState<'Bio' | 'Trips' | 'Settings'>('Bio');
+	const { data: session } = useSession();
+	const isAccount = session?.user.id === user?.id;
+	console.log(isAccount);
+	console.log('POOOOP');
+	if (error) {
+		return <div>Error go brrrrrr {error}</div>;
+	}
+	if (!user || !jobs || !hobbies || !trips) {
+		return <div>Oh no no user!</div>;
+	}
+	console.log(user);
+	console.log(trips);
+	return (
+		<div className="flex flex-row bg-blue-900 rounded-md h-full  space-x-3 ">
+			<div className="bg-blue-900 rounded-md  flex content-center flex5 flex-col   items-center w-1/3    ">
+				<img src={user.pfp} className="mt-3 bg-slate-500 rounded-full h-[100px] w-[100px]" />
+				<div className="text-white text-2xl mb-3">{user.name}</div>
+				<div className="flex justify-around w-full text- mb-4">
+					<div
+						className={`cursor-pointer ${
+							displayedInfo === 'Bio' ? 'border-b-2 border-x-0 border-t-0 border-white border-solid' : ''
+						}`}
+						onClick={() => {
+							selectDisplayedInfo('Bio');
+						}}
+					>
+						Bio
+					</div>
+					<div
+						className={`cursor-pointer ${
+							displayedInfo === 'Trips'
+								? 'border-b-2 border-x-0 border-t-0 border-white border-solid'
+								: ''
+						}`}
+						onClick={() => selectDisplayedInfo('Trips')}
+					>
+						Trips
+					</div>
+					{isAccount && (
+						<div
+							className={`cursor-pointer ${
+								displayedInfo === 'Settings'
+									? 'border-b-2 border-x-0 border-t-0 border-white border-solid'
+									: ''
+							}`}
+							onClick={() => selectDisplayedInfo('Settings')}
+						>
+							Settings
+						</div>
+					)}
+				</div>
+				{displayedInfo === 'Bio' && <Bio user={user} />}
+				{displayedInfo === 'Trips' && <Trips trips={trips} />}
+				{displayedInfo === 'Settings' && isAccount && <Settings user={user} jobs={jobs} hobbies={hobbies} />}
+			</div>
+			<div className="bg-indigo-800  w-full rounded-md flex h-full w-2/3">
+				{/* <LocationSearch /> */}
+				<RenderMap trips={trips} />;
+			</div>
+		</div>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({ req, res, query }) => {
+	console.log(query.id);
+	let userId = query.id[0] as string | null | undefined;
+	if (!userId) {
+		const session = await getServerSession(req, res, authOptions);
+		userId = session?.user.id;
+	}
+
+	if (userId) {
+		const u = prisma?.user.findFirst({ where: { id: userId }, include: { hobbies: true } });
+		const j = prisma?.job.findMany({});
+		const h = prisma?.hobby.findMany({});
+		const t = prisma?.trip.findMany({ where: { userId: userId }, include: { pictures: true } });
+
+		const [user, jobs, hobbies, trips] = await Promise.all([u, j, h, t]);
+		if (user && jobs && hobbies && trips) {
+			return {
+				props: {
+					user: {
+						id: user.id,
+						bio: user.bio ?? '',
+						pfp: user.image ?? '',
+						name: user.name ?? '',
+						job: user.jobName ?? '',
+						hobbies: user.hobbies.map((hobby) => hobby.hobbyName),
+					},
+					jobs: jobs.map((job) => job.value),
+					hobbies: hobbies.map((hobby) => hobby.value),
+					trips: trips.map((trip) => ({
+						date: trip.date.toLocaleDateString('en-us'),
+						desc: trip.desc,
+						location: trip.location,
+						lat: trip.lat,
+						lon: trip.lon,
+						urls: trip.pictures.map((pic) => pic.url),
+					})),
+				},
+			};
+		}
+	}
+	return { props: { error: 'Failed to load user info' } };
+};
+
+export default Account;
