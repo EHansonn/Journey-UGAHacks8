c57cc8f27517fa8f549d774147398232ed619f62
NicolasNewman <nanewman01@gmail.com>
2023-02-03 20:13:15 -0600

    added nextauth session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import '@/styles/global_preload.css';
 import 'antd/dist/reset.css';
 import '@/styles/global_postload.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />;
+		</SessionProvider>
+	);
 }
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,7 +4,6 @@ import NextAuth, { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
 	interface Session {
 		user: {
-			role: Role;
 			id: string;
 		} & DefaultSession['user'];
 	}
