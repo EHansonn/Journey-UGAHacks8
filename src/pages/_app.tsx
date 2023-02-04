8f127d3bfc8691b7c7589682f453accd866e7a90
NicolasNewman <nanewman01@gmail.com>
2023-02-03 18:30:22 -0600

    fixed undefined css file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import '@/styles/global_preload.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 import '@/styles/global_postload.css';
 
 export default function App({ Component, pageProps }: AppProps) {
