596f40d4905d5b7443ae12c38933272c16eba913
EHansonn <evanhanson02@gmail.com>
2023-02-05 00:16:05 -0500

    map modal buttons

--- components/maps/Map.tsx
+++ components/maps/Map.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Circle, GoogleMap, LoadScript, MarkerF, Marker, useJsApiLoader } from '@react-google-maps/api';
 import { Trip } from '@/pages/account';
 import { User } from '@/pages/account';
 import House from '../../public/house.png';
+import TripDataModal, { TripDataRef } from './TripDataModal';
 interface Props {
 	trips: Trip[];
 	user: User;
@@ -12,6 +13,8 @@ const Map: React.FC<Props> = ({ trips, user }) => {
 		googleMapsApiKey: 'AIzaSyBmL0gukE5saXobjQNHTXDgKwUegl4ikMU',
 		libraries: ['drawing', 'geometry', 'localContext', 'places', 'visualization'],
 	});
+
+	let whichMarkerClicked = 0;
 
 	const onLoad = (marker: any) => {
 		console.log('marker: ', marker);
@@ -27,25 +30,44 @@ const Map: React.FC<Props> = ({ trips, user }) => {
 		lng: -122.214,
 	};
 
+	const TripDataRef = useRef<TripDataRef>(null);
+	console.log('918391283123');
+	console.log(trips);
 	if (isLoaded) {
 		return (
 			// <LoadScript googleMapsApiKey="AIzaSyBmL0gukE5saXobjQNHTXDgKwUegl4ikMU">
-			<GoogleMap
-				mapContainerClassName=" w-screen h-full"
-				id="circle-example"
-				// mapContainerStyle={containerStyle}
-				center={center}
-				zoom={1.5}
-			>
-				<MarkerF
-					onLoad={onLoad}
-					icon={'https://cdn.discordapp.com/attachments/1071140153445331048/1071627727578337340/house2.png'}
-					position={{ lat: user.homeLat, lng: user.homeLon }}
-				/>
-				{trips.map((trip, i) => (
-					<MarkerF key={i} onLoad={onLoad} position={{ lat: trip.lat, lng: trip.lon }} />
-				))}
-			</GoogleMap>
+			<div>
+				<GoogleMap
+					mapContainerClassName=" w-screen h-full"
+					id="circle-example"
+					// mapContainerStyle={containerStyle}
+					center={center}
+					zoom={1.5}
+				>
+					<MarkerF
+						onLoad={onLoad}
+						icon={
+							'https://cdn.discordapp.com/attachments/1071140153445331048/1071638696148861038/house2.png'
+						}
+						position={{ lat: user.homeLat, lng: user.homeLon }}
+					/>
+					<TripDataModal ref={TripDataRef} />
+					{trips.map((trip, i) => (
+						<div key={i + 'hi mom'}>
+							<MarkerF
+								onClick={() => {
+									console.log('hi');
+									TripDataRef?.current?.showModal(true, trip);
+								}}
+								key={i}
+								onLoad={onLoad}
+								position={{ lat: trip.lat, lng: trip.lon }}
+							/>
+						</div>
+					))}
+				</GoogleMap>
+			</div>
+
 			// </LoadScript>
 		);
 	}
