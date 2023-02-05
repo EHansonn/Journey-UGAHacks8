160dbfd6cff4141e9b9eb29092ea741ddc64a0f3
NicolasNewman <nanewman01@gmail.com>
2023-02-04 18:59:26 -0600

    alignment adjustments

--- components/account-components/Bio.tsx
+++ components/account-components/Bio.tsx
@@ -7,13 +7,13 @@ interface Props {
 const Bio: React.FC<Props> = ({ user }) => {
 	return (
 		<>
-			<div className="text-white grid grid-cols-2">
+			<div className="text-white grid grid-cols-2 mx-3">
 				<div className="col-span-2 mb-3">{user.bio}</div>
-				<div>Job:</div>
+				<div className="justify-self-center w-20 text-end mb-2">Job:</div>
 				<div>{user.job}</div>
-				<div>Home:</div>
+				<div className="justify-self-center w-20 text-end mb-2">Home:</div>
 				<div>Tamriel</div>
-				<div>Hobbies:</div>
+				<div className="justify-self-center w-20 text-end mb-2">Hobbies:</div>
 				<div>{user.hobbies.join(', ')}</div>
 			</div>
 		</>
